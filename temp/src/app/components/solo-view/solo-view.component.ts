d5744eed830ae6afa3826c6985f2bc6f7701d076
Charlie Jindra <charlessjindra@gmail.com>
2023-12-06 20:11:02 -0500

    inspect feature

--- temp/src/app/components/solo-view/solo-view.component.ts
+++ temp/src/app/components/solo-view/solo-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BinService } from 'src/app/services/bin/bin.service';
 import { AbstractSoloService } from 'src/app/services/solo-view/abstract.solo-view.service';
 
@@ -9,17 +10,22 @@ import { AbstractSoloService } from 'src/app/services/solo-view/abstract.solo-vi
 })
 export class SoloViewComponent implements OnInit {
 
-  constructor(public soloService: AbstractSoloService) { }
+  constructor(public soloService: AbstractSoloService, public router: Router) { }
 
   public title: string;
   public imgUrl: string;
   public modify: boolean;
+  public inspect: boolean = false;
 
   ngOnInit(): void {
   }
 
   public modifyMode() {
     this.modify = !this.modify;
+  }
+
+  public inspectMode() {
+    this.inspect = !this.inspect;
   }
 
 }
